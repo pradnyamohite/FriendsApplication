eb33370a8fa2b65df5f81683025f4ed67023b3b8
pradnyamohite <69071337+pradnyamohite@users.noreply.github.com>
2021-11-03 22:31:05 +0530

    CR:Pradnya
    login_create page done

---
+++ Models/frd_create.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FriendApplication.Models
+{
+    public class frd_create
+    {
+        public string frd_fname { get; set; }
+
+        public string frd_lname { get; set; }
+
+        public int frd_num { get; set; }
+
+        public int frd_email { get; set; }
+    }
+}
---
+++ Models/frd_login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FriendApplication.Models
+{
+    public class frd_login
+    {
+        public int frd_num { get; set; }
+
+        public string frd_pass { get; set; }
+    }
+}
